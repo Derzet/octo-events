9da3b03f6b7c3ebb1b047328256a1d711ff19922
Matheus Herminio <voyager_mt@hotmail.com>
2020-09-19 22:45:42 -0300

    #14 Change ddl-auto create-drop and fix syntax initial data to test.

--- octo-events/src/test/resources/data.sql
+++ octo-events/src/test/resources/data.sql
@@ -1,2 +1,2 @@
- INSERT INTO issue (id) VALUES (1)
- INSERT into  event (id, action, created_at, id_issue) VALUES (null, 'created', '2020-09-01 20:33:11.806', 1)
+ INSERT INTO issue (id) VALUES (1);
+ INSERT INTO event (id, action, created_at, id_issue) VALUES (null, 'created', '2020-09-01 20:33:11.806', 1);
